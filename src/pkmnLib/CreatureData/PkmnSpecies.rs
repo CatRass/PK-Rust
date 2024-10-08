20c1939206643c054c52c71e73909a9a0f13ac66
Darrel Malkin <catrassyt@gmail.com>
2024-10-08 15:45:28 +1100

    Added Test for PkmnSpecies - Added test parse_testCorrectPokemon

--- src/pkmnLib/CreatureData/PkmnSpecies.rs
+++ src/pkmnLib/CreatureData/PkmnSpecies.rs
@@ -2,6 +2,13 @@ use std::fs;
 use super::Type;
 
 #[derive(Debug)]
+/**
+ * The object that stores a the Pokemon Species
+ * index: The index given to a pokemon sprite (0x01 being Rhydon)
+ * pokedex: The number of the pokemon in the pokedex
+ * name: The string name of the species
+ * typing: an array holding the two types of a species
+ */
 pub struct Species {
     index: i16,
     pokedex: i16,
@@ -15,7 +22,7 @@ impl Species {
         let mut parsedSpecies: &str = "No Pokemon found";
 
         let hexIndex = format!("0x{:02X?}",index);
-        // println!("{}",hexIndex);
+        println!("{}",hexIndex);
         
         for line in speciesFile.lines() {
             if line.contains(&hexIndex) {
@@ -37,4 +44,47 @@ impl Species {
 
         return Species{index,pokedex,name,typing};
     }
+
+    pub fn getIndex(&self) -> &i16 {
+        return &self.index;
+    }
+
+    pub fn getPokedex(&self) -> &i16 {
+        return &self.pokedex;
+    }
+
+    pub fn getName(&self) -> &String {
+        return &self.name;
+    }
+
+    pub fn getTyping(&self) -> &[Type;2] {
+        return &self.typing;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn parse_testCorrectPokemon() {
+        // Id of the pokemon
+        let id = 0x99;
+        let parsedSpecies: Species = Species::parse(id);
+
+        let correctSpecies:Species = Species {
+            index: 0x99, 
+            pokedex: 1, 
+            name: "Bulbasaur".to_string(), 
+            typing: [Type::Grass, Type::Poison]};
+
+        // Assert Name
+        assert_eq!(&correctSpecies.getName(), &parsedSpecies.getName());
+        // Assert Species ID
+        assert_eq!(&correctSpecies.getIndex(), &parsedSpecies.getIndex());
+        // Assert Pokedex
+        assert_eq!(&correctSpecies.getPokedex(), &parsedSpecies.getPokedex());
+    }
+
 }
