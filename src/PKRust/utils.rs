a7b46cd2e4c1a12721502884540777fbc8032e16
Darrel Malkin <catrassyt@gmail.com>
2024-10-30 19:06:37 +1100

    Added Tests (Partial) and Implementation for Text Encoding - Implemented function utils::textEncode() - Added test textEncode_UppercaseLetter() - Added test textEncode_CapitalWord() - Added test textEncode_LowercaseLetter() - Added test textEncode_LowercaseWord() - Added test textEncode_MixedcaseWord() NOTE: I have realised that we do not test for the special character 'é'. Tests in next commit

--- src/PKRust/utils.rs
+++ src/PKRust/utils.rs
@@ -83,11 +83,78 @@ pub fn textDecode(encoded: &[i16; 11]) -> String{
     return String::from_utf8(name).unwrap();
 }
 
-// /// Encodes text into the character encoding used by Gen 1
-// /// **TODO**: Implement this!
-// fn textEncode(decoded: &String) -> [i16; 11]{
-//     todo!("Finish this, preferrably optimised");
-// }
+/// Encodes text into the character encoding used by Gen 1
+fn textEncode(decoded: &String) -> [i16; 11]{
+    let mut encoded: [i16; 11] = [0; 11];
+
+    for index in 0..10 {
+        let currChar = decoded.chars().nth(index).unwrap();
+        encoded[index] = match currChar {
+            'A' => 0x80,
+            'B' => 0x81,
+            'C' => 0x82,
+            'D' => 0x83,
+            'E' => 0x84,
+            'F' => 0x85,
+            'G' => 0x86,
+            'H' => 0x87,
+            'I' => 0x88,
+            'J' => 0x89,
+            'K' => 0x8A,
+            'L' => 0x8B,
+            'M' => 0x8C,
+            'N' => 0x8D,
+            'O' => 0x8E,
+            'P' => 0x8F,
+            'Q' => 0x90,
+            'R' => 0x91,
+            'S' => 0x92,
+            'T' => 0x93,
+            'U' => 0x94,
+            'V' => 0x95,
+            'W' => 0x96,
+            'X' => 0x97,
+            'Y' => 0x98,
+            'Z' => 0x99,
+            '(' => 0x9A,
+            ')' => 0x9B,
+            ':' => 0x9C,
+            ';' => 0x9D,
+            '[' => 0x9E,
+            ']' => 0x9F,
+            'a' => 0xA0,
+            'b' => 0xA1,
+            'c' => 0xA2,
+            'd' => 0xA3,
+            'e' => 0xA4,
+            'f' => 0xA5,
+            'g' => 0xA6,
+            'h' => 0xA7,
+            'i' => 0xA8,
+            'j' => 0xA9,
+            'k' => 0xAA,
+            'l' => 0xAB,
+            'm' => 0xAC,
+            'n' => 0xAD,
+            'o' => 0xAE,
+            'p' => 0xAF,
+            'q' => 0xB0,
+            'r' => 0xB1,
+            's' => 0xB2,
+            't' => 0xB3,
+            'u' => 0xB4,
+            'v' => 0xB5,
+            'w' => 0xB6,
+            'x' => 0xB7,
+            'y' => 0xB8,
+            'z' => 0xB9,
+            'é' => 0xBA,
+            _   => 0x00 
+        }
+    }
+
+    return encoded;
+}
 
 pub fn integrityCheck(saveFile: &Vec<u8>) -> bool {
     // We will be using the "easy" way, as shown in the
@@ -126,7 +193,7 @@ mod tests {
     }
 
     #[test]
-    fn textDecpde_CapitalWord() {
+    fn textDecode_CapitalWord() {
         // Word "POKEMON"
         let word: [i16; 11] = [0x8F, 0x8E, 0x8A, 0x84, 0x8C, 0x8E, 0x8D, 0x00, 0x00, 0x00, 0x00];
         assert_eq!(textDecode(&word), "POKEMON    ");
@@ -189,5 +256,53 @@ mod tests {
 
         assert_eq!(fileHasIntegrity, false);
     }
+
+    #[test]
+    fn textEncode_UppercaseLetter() {
+        // Letter "P"
+        let decodedText = String::from("P          ");
+
+        let mut letterList: [i16; 11] = [0x00; 11];
+        letterList[0] = 0x8F;
+        assert_eq!(textEncode(&decodedText), letterList);
+    }
+
+    #[test]
+    fn textEncode_CapitalWord() {
+        // Word "POKEMON"
+        let decodedText = String::from("POKEMON    ");
+        let word: [i16; 11] = [0x8F, 0x8E, 0x8A, 0x84, 0x8C, 0x8E, 0x8D, 0x00, 0x00, 0x00, 0x00];
+
+        assert_eq!(textEncode(&decodedText), word);
+    }
+
+    #[test]
+    fn textEncode_LowercaseLetter() {
+        // Letter "p"
+        let decodedText = String::from("p          ");
+
+        let mut letterList: [i16; 11] = [0x00; 11];
+        letterList[0] = 0xAF;
+        assert_eq!(textEncode(&decodedText), letterList);
+    }
+
+    #[test]
+    fn textEncode_LowercaseWord() {
+        // Word "pokemon"
+        let decodedText = String::from("pokemon    ");
+
+        let word: [i16; 11] = [0xAF, 0xAE, 0xAA, 0xA4, 0xAC, 0xAE, 0xAD, 0x00, 0x00, 0x00, 0x00];
+        assert_eq!(textEncode(&decodedText), word);
+    }
+
+    #[test]
+    fn textEncode_MixedcaseWord() {
+        // Word "Pokemon"
+        let decodedText = String::from("Pokemon    ");
+
+        let word: [i16; 11] = [0x8F, 0xAE, 0xAA, 0xA4, 0xAC, 0xAE, 0xAD, 0x00, 0x00, 0x00, 0x00];
+        assert_eq!(textEncode(&decodedText), word);
+    }
+
 
 }
