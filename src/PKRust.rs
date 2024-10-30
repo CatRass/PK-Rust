64a969dd11eb3e095b8e393039be072aaceb0969
Darrel Malkin <catrassyt@gmail.com>
2024-10-30 17:03:22 +1100

    CreatureData Naming Change, and Getters for Save - Renamed CreatureData module to creatureData - Added getters for all variables in Save struct

--- src/PKRust.rs
+++ src/PKRust.rs
@@ -1,6 +1,6 @@
 #![allow(non_snake_case)]
 
-pub mod CreatureData;
+pub mod creatureData;
 pub mod addresses;
 pub mod saveLoader;
 pub mod utils;
--- src/PKRust/saveLoader.rs
+++ src/PKRust/saveLoader.rs
@@ -1,13 +1,14 @@
 use std::fs;
 use std::path::PathBuf;
 
-use super::CreatureData::pokemonMove::Move;
-use super::CreatureData::pokemon::*;
+use super::creatureData::pokemonMove::Move;
+use super::creatureData::pokemon::*;
 use super::addresses::*;
 use super::utils::{textDecode, integrityCheck};
 
 
 #[derive(Debug)]
+#[allow(dead_code)]
 pub struct Save {
     trainer: String,
     money: u32,
@@ -18,6 +19,7 @@ pub struct Save {
     pc: Vec<Vec<Pokemon>>
 }
 
+#[allow(dead_code)]
 impl Save {
 
     pub fn new() -> Save {
@@ -48,12 +50,12 @@ impl Save {
             return Err(String::from("Error: File does not seem to be a Gen 1 Save File"));
         }
 
-        let pc = Self::getPCBoxes(&save);
+        let pc = Self::getPCBoxesFromSave(&save);
 
-        let money = Self::getMoney(&save);
-        let id = Self::getTrainerID(&save);
-        let party:  Vec<Pokemon> = Self::getParty(&save);
-        let trainer = textDecode(&Self::getName(&save));
+        let money = Self::getMoneyFromSave(&save);
+        let id = Self::getTrainerIDFromSave(&save);
+        let party:  Vec<Pokemon> = Self::getPartyFromSave(&save);
+        let trainer = textDecode(&Self::getNameFromSave(&save));
 
         return Ok(Save{trainer, money, id, party, pc});
 
@@ -76,12 +78,39 @@ impl Save {
         println!("=================\n");
     }
 
+    /// Getter for Trainer Name in Save
+    pub fn getTrainerName(&self) -> &String {
+        return &self.trainer;
+    }    
+
+    /// Getter for Money in Save
+    pub fn getMoney(&self) -> &u32 {
+        return &self.money;
+    }
+
+    /// Getter for Trainer ID in Save
+    pub fn getTrainerID(&self) -> &i32 {
+        return &self.id;
+    }
+
+    /// Getter for Party Pokemon
+    pub fn getParty(&self) -> &Vec<Pokemon> {
+        return &self.party;
+    }
+
+    /// Getter for PC Boxes
+    pub fn getPCBoxes(&self) -> &Vec<Vec<Pokemon>> {
+        return &self.pc;
+    }
+
+    // ========   SAVE FILE RETRIEVAL    ======== 
+
     /// Retrieves the name from the save file
     /// 
     /// Since most Pokemon games use character encoding, we have to decode it.
     /// 
     /// [Gen 1](https://bulbapedia.bulbagarden.net/wiki/Character_encoding_(Generation_I))
-    fn getName(save: &Vec<u8>) -> [i16;11] {
+    fn getNameFromSave(save: &Vec<u8>) -> [i16;11] {
         let mut name: [i16; 11] = [0; 11];
         for num in 0..11 {
             name[num] = format!("{}",save[NAME_ADDR+num]).parse::<i16>().unwrap();
@@ -90,20 +119,20 @@ impl Save {
     }
 
     /// Retrieves the amount of money the player has
-    fn getMoney(save: &Vec<u8>) -> u32{
+    fn getMoneyFromSave(save: &Vec<u8>) -> u32{
         return format!("{:02X}{:02X}{:02X}",save[MONEY_ADDR],save[MONEY_ADDR+1],save[MONEY_ADDR+2])
         .parse::<u32>()
         .unwrap();
     }
 
     /// Retrieves the trainer ID
-    fn getTrainerID(save: &Vec<u8>) -> i32 {
+    fn getTrainerIDFromSave(save: &Vec<u8>) -> i32 {
         let stringID = format!("{:02X}{:02X}",save[ID_ADDR],save[ID_ADDR+1]);
         return i32::from_str_radix(&stringID, 16).unwrap();
     }
 
     /// Retrieves the players party of Pokemon
-    fn getParty(save: &Vec<u8>) -> Vec<Pokemon> {
+    fn getPartyFromSave(save: &Vec<u8>) -> Vec<Pokemon> {
         let mut party:  Vec<Pokemon> = Vec::new();
 
         for creature in 0..save[PARTY_ADDR] as usize {
@@ -111,20 +140,20 @@ impl Save {
             let nickAddress: usize = PARTY_ADDR + NICK_OFF + (creature * 0xB);
 
             // Get current HP
-            let hp = Self::getPokemonHP(&save,&pkmnAddress);
+            let hp = Self::getPokemonHPFromSave(&save,&pkmnAddress);
             // Nickname Obtaining code
-            let nickname = Self::getPokemonNick(&save, &nickAddress);
+            let nickname = Self::getPokemonNickFromSave(&save, &nickAddress);
             // Moves Obtaining code
-            let moves = Self::getPokemonMoves(&save,&pkmnAddress);
+            let moves = Self::getPokemonMovesFromSave(&save,&pkmnAddress);
             // EV Obtaining code
-            let evs: [u16;5] = Self::getPokemonEVs(&save,&pkmnAddress);
+            let evs: [u16;5] = Self::getPokemonEVsFromSave(&save,&pkmnAddress);
             // Stat Obtaining Code
-            let stats: [u16;5] = Self::getPokemonStats(&save,&pkmnAddress);
+            let stats: [u16;5] = Self::getPokemonStatsFromSave(&save,&pkmnAddress);
             // IV Obtaining Code
-            let ivs: [u16;5] = Self::getPokemonIVs(&save,&pkmnAddress);
+            let ivs: [u16;5] = Self::getPokemonIVsFromSave(&save,&pkmnAddress);
             // Original Trainer Obtaining Code
-            let ot = Self::getPokemonOTID(&save,&pkmnAddress);
-            let otn = Self::getPokemonOTName(&save, &(pkmnAddress+OTN_OFF));
+            let ot = Self::getPokemonOTIDFromSave(&save,&pkmnAddress);
+            let otn = Self::getPokemonOTNameFromSave(&save, &(pkmnAddress+OTN_OFF));
 
             party.push(Pokemon::get(    save[pkmnAddress] as i16,
                                         save[pkmnAddress+0x21] as i8,
@@ -143,7 +172,7 @@ impl Save {
     }
 
     /// Retrieves all of the players PC boxes
-    fn getPCBoxes(save: &Vec<u8>) -> Vec<Vec<Pokemon>>{
+    fn getPCBoxesFromSave(save: &Vec<u8>) -> Vec<Vec<Pokemon>>{
         let mut boxes: Vec<Vec<Pokemon>> = Vec::new();
 
         for pcBox in 0..12 as usize {
@@ -158,14 +187,14 @@ impl Save {
                 let nickAddress = currAddr + PC_NICK_OFF+ (creature*0xB);
 
                 let currSpecies: i16 = save[pkmnAddress] as i16;
-                let hp = Self::getPokemonHP(&save, &pkmnAddress);
-                let ot = Self::getPokemonOTID(&save, &pkmnAddress);
-                let moves = Self::getPokemonMoves(&save,&pkmnAddress);
-                let nickname = Self::getPokemonNick(&save, &nickAddress);
-                let evs: [u16;5] = Self::getPokemonEVs(&save,&pkmnAddress);
-                let ivs: [u16;5] = Self::getPokemonIVs(&save,&pkmnAddress);
+                let hp = Self::getPokemonHPFromSave(&save, &pkmnAddress);
+                let ot = Self::getPokemonOTIDFromSave(&save, &pkmnAddress);
+                let moves = Self::getPokemonMovesFromSave(&save,&pkmnAddress);
+                let nickname = Self::getPokemonNickFromSave(&save, &nickAddress);
+                let evs: [u16;5] = Self::getPokemonEVsFromSave(&save,&pkmnAddress);
+                let ivs: [u16;5] = Self::getPokemonIVsFromSave(&save,&pkmnAddress);
                 let level: i8 = save[pkmnAddress+0x03] as i8;
-                let otn = Self::getPokemonOTName(&save, &(currAddr+PC_TRAINER_OFF+(creature*0xB)));
+                let otn = Self::getPokemonOTNameFromSave(&save, &(currAddr+PC_TRAINER_OFF+(creature*0xB)));
                 // println!("Original Trainer name at: {:X}",currAddr+PC_TRAINER_OFF+(creature*0xB));
 
                 // TODO: Finish this so it uses the box trick to calculate the proper stats.
@@ -196,7 +225,7 @@ impl Save {
     }
 
     /// Function for retrieving a Pokemons Original Trainers ID
-    fn getPokemonOTID(save: &Vec<u8>, currAddr: &usize) -> u16{
+    fn getPokemonOTIDFromSave(save: &Vec<u8>, currAddr: &usize) -> u16{
         return u16::from_str_radix( 
             &format!("{:02X}{:02X}", save[currAddr+OT_OFF],save[currAddr+OT_OFF+1]),
             16
@@ -207,7 +236,7 @@ impl Save {
     /// 
     /// **Note**: There is a current bug where extra "garbage data" is added to OT Names.
     /// This most likely to do with the function textDecode note taking into account control characters.
-    fn getPokemonOTName(save: &Vec<u8>, currAddr: &usize) -> String {
+    fn getPokemonOTNameFromSave(save: &Vec<u8>, currAddr: &usize) -> String {
         let mut encodedName: [i16;11] = [0;11];
 
         for char in 0..11 {
@@ -218,7 +247,7 @@ impl Save {
     }
 
     /// Function for retrieving the Pokemons current Health Points
-    fn getPokemonHP(save: &Vec<u8>, currAddr: &usize) -> i16{
+    fn getPokemonHPFromSave(save: &Vec<u8>, currAddr: &usize) -> i16{
         return i16::from_str_radix(
             &format!("{:02X}{:02X}",save[currAddr+HP_OFF],save[currAddr+HP_OFF+1]), 
             16
@@ -228,7 +257,7 @@ impl Save {
     /// Function for retrieving a Pokemons Nickname.
     /// 
     /// **Note**: This function will automatically decode it into a String
-    fn getPokemonNick(save: &Vec<u8>,currAddr: &usize) -> String {
+    fn getPokemonNickFromSave(save: &Vec<u8>,currAddr: &usize) -> String {
         let mut encodedNick: [i16; 11]= [0; 11];
             for num in 0..11 {
                 encodedNick[num] = format!("{}",save[currAddr+num]).parse::<i16>().unwrap();
@@ -237,7 +266,7 @@ impl Save {
     }
 
     /// Function for retrieving a Pokemons base stats
-    fn getPokemonStats(save: &Vec<u8>,currAddr: &usize) -> [u16;5] {
+    fn getPokemonStatsFromSave(save: &Vec<u8>,currAddr: &usize) -> [u16;5] {
         let mut stats: [u16;5] = [0; 5];
             for stat in 0..5 {
                 let currAddr = currAddr+STAT_OFF+(stat*2);
@@ -251,7 +280,7 @@ impl Save {
     }
 
     /// Function for retrieving a Pokemons Effort Values
-    fn getPokemonEVs(save: &Vec<u8>,currAddr: &usize) -> [u16;5] {
+    fn getPokemonEVsFromSave(save: &Vec<u8>,currAddr: &usize) -> [u16;5] {
         let mut evs: [u16;5] = [0; 5];
             for stat in 0..5 {
                 let currAddr = currAddr+EV_OFF+(stat*2);
@@ -265,7 +294,7 @@ impl Save {
     }
 
     /// Function for retrieving data about Pokemons moves.
-    fn getPokemonMoves(save: &Vec<u8>, currAddr: &usize) -> Vec<Move>{
+    fn getPokemonMovesFromSave(save: &Vec<u8>, currAddr: &usize) -> Vec<Move>{
     let mut returnVec: Vec<Move> = Vec::new();
     let moveAddr = currAddr + MOVE_OFF;
 
@@ -285,12 +314,11 @@ impl Save {
     return returnVec;
 }
 
-
     /// Function for retrieving a Pokemons Individual Values 
     /// (Also known as Determinant Values)
     /// 
     /// **TODO**: Figure out a better way to split into fours
-    fn getPokemonIVs(save: &Vec<u8>,currAddr: &usize) -> [u16;5]{
+    fn getPokemonIVsFromSave(save: &Vec<u8>,currAddr: &usize) -> [u16;5]{
         let allIVs = format!("{:08b}{:08b}",save[currAddr+IV_OFF],save[currAddr+IV_OFF+1]);
         let (half, half2) = allIVs.split_at(8);
         let ((atk, def), (spd, spc)) = (half.split_at(4), half2.split_at(4));
