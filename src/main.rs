8123d8d867af2ceff6c93c75b85f156bac9b794e
Darrel Malkin <catrassyt@gmail.com>
2024-10-31 16:35:06 +1100

    Changed OS-based Line Ending Trimming - Changed the OS-based line ending trimming, and used just trim()

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,8 @@ fn main() {
     let mut saveLocInput = String::new();
     io::stdin().read_line(&mut saveLocInput).expect("Error recieving User Input");
 
-    let mut saveLoc = "";
     // The read_line collects line endings, so we need to remove them.
-    // This varies across different OS' though, so I've split it
-    // into Unix and Windows
-    if cfg!(windows) {
-        saveLoc = saveLocInput.strip_suffix("\r\n").unwrap();
-    } else if cfg!(unix) {
-        saveLoc = saveLocInput.strip_suffix("\n").unwrap();
-    }
+    let saveLoc = saveLocInput.trim();
 
     let saveFile = Save::load(saveLoc).unwrap();
     saveFile.print();
